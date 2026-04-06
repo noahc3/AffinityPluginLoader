1f4d1acb9af0eab98c171ad5ab968ff9596b2118
noahc3 <noahcuroe@gmail.com>
2026-04-05 21:55:31 -0500

    Add settings toggle for force synchronous font enumeration

--- WineFix/WineFixPlugin.cs
+++ WineFix/WineFixPlugin.cs
@@ -12,10 +12,16 @@ namespace WineFix
     {
         public const string PluginId = "winefix";
         public const string ColorPickerModeKey = "color_picker_mode";
+        public const string SettingForceSyncFontEnum = "force_sync_font_enum";
 
         public override PluginSettingsDefinition DefineSettings()
         {
             return new PluginSettingsDefinition(PluginId)
+                .AddSection("Crash Fixes")
+                .AddBool(SettingForceSyncFontEnum, "Force synchronous font enumeration",
+                    defaultValue: true,
+                    restartRequired: true,
+                    description: "Disable parallel font enumeration to significantly reduce frequency of startup crashes. May increase application startup time on systems with lots of fonts.")
                 .AddSection("Patches")
                 .AddEnum(ColorPickerModeKey, "Color Picker Mode",
                     new List<EnumOption>
@@ -31,6 +37,12 @@ namespace WineFix
         {
             context.Patch("MainWindowLoaded fix",
                 h => Patches.MainWindowLoadedPatch.ApplyPatches(h));
+
+            if (context.Settings.GetEffectiveValue<bool>(SettingForceSyncFontEnum))
+            {
+                context.Patch("FontEnumeration fix",
+                    h => Patches.FontEnumerationPatch.ApplyPatches(h));
+            }
 
             context.Patch("ColorPicker Wayland fix",
                 h => Patches.ColorPickerWaylandPatch.ApplyPatches(h));
