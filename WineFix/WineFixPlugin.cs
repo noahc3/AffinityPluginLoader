e0639a20d8c4a93c5750a093fdf682b60eb70d59
noahc3 <noahcuroe@gmail.com>
2026-04-05 22:46:47 -0400

    Force disable parallel font enumeration to reduce startup crashes (#34)

--- WineFix/WineFixPlugin.cs
+++ WineFix/WineFixPlugin.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using AffinityPluginLoader;
+using AffinityPluginLoader.Settings;
 
 namespace WineFix
 {
@@ -8,10 +9,28 @@ namespace WineFix
     /// </summary>
     public class WineFixPlugin : AffinityPlugin
     {
+        public const string SettingForceSyncFontEnum = "force_sync_font_enum";
+
+        public override PluginSettingsDefinition DefineSettings()
+        {
+            return new PluginSettingsDefinition("winefix")
+                .AddSection("Crash Fixes")
+                .AddBool(SettingForceSyncFontEnum, "Force synchronous font enumeration",
+                    defaultValue: true,
+                    restartRequired: true,
+                    description: "Disable parallel font enumeration to significantly reduce frequency of startup crashes. May increase application startup time on systems with lots of fonts.");
+        }
+
         public override void OnPatch(Harmony harmony, IPluginContext context)
         {
             context.Patch("MainWindowLoaded fix",
                 h => Patches.MainWindowLoadedPatch.ApplyPatches(h));
+
+            if (context.Settings.GetEffectiveValue<bool>(SettingForceSyncFontEnum))
+            {
+                context.Patch("FontEnumeration fix",
+                    h => Patches.FontEnumerationPatch.ApplyPatches(h));
+            }
         }
     }
 }
